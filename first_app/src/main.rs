26d33d6711a0ba580dab2ab55167f21d8dce0453
kunal171 <kunal@rapidinnovation.dev>
2022-08-12 21:46:14 +0530

    first server

--- first_app/src/main.rs
+++ first_app/src/main.rs
@@ -1,29 +1,33 @@
-use actix_web::{web, App, HttpServer};
-use std::sync::Mutex;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+// this function could be located in a different module
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
 }
 
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
-
-    format!("Request number: {counter}") // <- response with count
+// this function could be located in a different module
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Note: web::Data created _outside_ HttpServer::new closure
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-
-    HttpServer::new(move || {
-        // move counter into the closure
+    HttpServer::new(|| {
         App::new()
-            .app_data(counter.clone()) // <- register the created data
-            .route("/", web::get().to(index))
+            .configure(config)
+            .service(web::scope("/api").configure(scoped_config))
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("/") }),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
