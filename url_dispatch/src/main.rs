e525fc57558f7d9978e42d9ca4afbfab8a38ade6
kunal171 <kunalkunal63@gmail.com>
2023-11-30 19:55:00 +0530

    errors

---
+++ url_dispatch/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, App, HttpResponse, HttpServer, guard};
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(web::resource("/prefix").to(index))
+        .service(
+            web::resource("/user/{name}")
+                .name("user_detail")
+                .guard(guard::Header("content-type", "application/json"))
+                .route(web::get().to(HttpResponse::Ok))
+                .route(web::put().to(HttpResponse::Ok)),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
